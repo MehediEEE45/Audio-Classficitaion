430ec75610ce5681942955f6df3d913d341d0232
MMhMehedi <145740784+MehediEEE45@users.noreply.github.com>
2025-11-03 12:44:26 +0600

    Update display.c

--- Src/display.c
+++ Src/display.c
@@ -34,7 +34,26 @@ void LCD_Config(void)
 }
 
 
+void setFont(char size)
+{
+    size = tolower(size); // supports 'S' or 's', etc.
 
+    switch (size)
+    {
+    case 's': // small
+        BSP_LCD_SetFont(&Font12);
+        break;
+    case 'm': // medium
+        BSP_LCD_SetFont(&Font16);
+        break;
+    case 'l': // large
+        BSP_LCD_SetFont(&Font24);
+        break;
+    default: // default font
+        BSP_LCD_SetFont(&Font20);
+        break;
+    }
+}
 
 
 void text(int x, int y, char ps, char text_str[], char text_color, char scr_color)
@@ -75,4 +94,43 @@ void text(int x, int y, char ps, char text_str[], char text_color, char scr_colo
         BSP_LCD_DisplayStringAt(x, y, (uint8_t*)buf, RIGHT_MODE);
     else
         BSP_LCD_DisplayStringAt(x, y, (uint8_t*)buf, CENTER_MODE);
+}
+
+
+
+
+void multitext(int x, int y, char ps, char text_str[], char text_color)
+{
+    char buf[64];
+    ps = tolower(ps);
+    text_color = tolower(text_color);
+
+    // ----- Set Text Color -----
+    if (text_color == 'b')
+        BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
+    else if (text_color == 'r')
+        BSP_LCD_SetTextColor(LCD_COLOR_RED);
+    else if (text_color == 'g')
+        BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
+    else
+        BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+
+    // ----- Display each line -----
+    int line_y = y; // starting y position
+    char *line = strtok(text_str, "\n"); // split by \n
+
+    while(line != NULL)
+    {
+        snprintf(buf, sizeof(buf), "%s", line);
+
+        if(ps == 'l')
+            BSP_LCD_DisplayStringAt(x, line_y, (uint8_t*)buf, LEFT_MODE);
+        else if(ps == 'r')
+            BSP_LCD_DisplayStringAt(x, line_y, (uint8_t*)buf, RIGHT_MODE);
+        else
+            BSP_LCD_DisplayStringAt(x, line_y, (uint8_t*)buf, CENTER_MODE);
+
+        line_y += BSP_LCD_GetFont()->Height; // move down for next line
+        line = strtok(NULL, "\n");
+    }
 }
